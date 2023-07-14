a43128b131933ad75267dd31061ed6c6690864ef
Josh Babb <sneurlax@gmail.com>
2023-07-14 12:11:45 -0500

    WIP example
    need to authenticate

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+// example app deps, not necessarily needed for tor usage
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_socks_proxy/socks_proxy.dart'; // just for example; can use any socks5 proxy package, pick your favorite.
 import 'package:tor/tor.dart';
 
 void main() {
@@ -18,11 +21,13 @@ class _MyAppState extends State<MyApp> {
   late int sumResult;
   late Future<int> sumAsyncResult;
 
+  final portController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
 
-   // Create the Tor class
+    // Create the Tor class
     dynamic tor = Tor();
 
     // Start the Tor daemon
@@ -32,6 +37,13 @@ class _MyAppState extends State<MyApp> {
     // sumAsyncResult = Future.delayed(Duration.zero, () {
     //   return 0;
     // }); // tor.sumAsync(3, 4);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    portController.dispose();
+    super.dispose();
   }
 
   @override
@@ -49,11 +61,45 @@ class _MyAppState extends State<MyApp> {
             child: Column(
               children: [
                 const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code must be built by running your platform\'s respective build script, see README.md.',
+                  'Enter the port of your Tor daemon/SOCKS5 proxy and press connect'
+                  'See the console logs for your port or ~/Documents/tor/tor.log',
                   style: textStyle,
                   textAlign: TextAlign.center,
                 ),
+                spacerSmall,
+                TextField(
+                  controller: portController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'SOCKS5 proxy port',
+                  ),
+                ),
+                spacerSmall,
+                TextButton(
+                    onPressed: () async {
+                      // proxy -> "SOCKS5/SOCKS4/PROXY username:password@host:port;" or "DIRECT"
+                      final http = createProxyHttpClient()
+                        ..findProxy =
+                            (url) => "SOCKS5 127.0.0.1:${portController.text}";
+                      http
+                          .getUrl(Uri.parse(
+                              'https://raw.githubusercontent.com/tayoji-io/socks_proxy/master/README.md'))
+                          .then((value) {
+                            return value.close();
+                          })
+                          .then((value) {
+                            return value.transform(utf8.decoder);
+                          })
+                          .then((value) {
+                            return value.fold(
+                                '',
+                                (dynamic previous, element) =>
+                                    previous + element);
+                          })
+                          .then((value) => print(value))
+                          .catchError((e) => print(e));
+                    },
+                    child: Text("connect")),
                 // TODO make test interactive (button, status indicator)
                 // spacerSmall,
                 // Text(
