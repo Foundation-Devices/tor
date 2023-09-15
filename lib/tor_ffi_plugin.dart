0e63d0a6faa753e0248d5f66da557aae1eebfb0a
julian <julian@cypherstack.com>
2023-09-15 12:48:32 -0600

    Refactor lib API to make it easier to use without changing core functionality. Removed/commented out functions/parameters that did not actually do anything.

--- lib/tor_ffi_plugin.dart
+++ lib/tor_ffi_plugin.dart
@@ -9,10 +9,10 @@ import 'dart:isolate';
 import 'dart:math';
 
 import 'package:ffi/ffi.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:tor_ffi_plugin/tor_ffi_plugin_bindings_generated.dart';
 
-DynamicLibrary load(name) {
+DynamicLibrary _load(name) {
   if (Platform.isAndroid || Platform.isLinux) {
     return DynamicLibrary.open('lib$name.so');
   } else if (Platform.isIOS || Platform.isMacOS) {
@@ -34,86 +34,146 @@ class NotSupportedPlatform implements Exception {
   NotSupportedPlatform(String s);
 }
 
+enum TorStatus {
+  on,
+  starting,
+  off;
+}
+
 class Tor {
+  /// Private constructor for the Tor class.
+  Tor._() {
+    _lib = _load(_libName);
+    if (kDebugMode) {
+      print("Native tor library loaded!");
+    }
+  }
+
+  /// Singleton instance of the Tor class.
+  static final Tor instance = Tor._();
+
   static const String _libName = "tor_ffi_plugin";
   static late DynamicLibrary _lib;
 
-  Pointer<Int> _clientPtr = nullptr;
-
-  /// Flag to indicate that a Tor should be started.
-  bool get started => _started;
-
-  /// Getter for the started flag.
-  bool _started = false;
-
-  /// Flag to indicate that Tor has started.
-  bool _enabled = false;
-
-  /// Getter for the enabled flag.
-  bool get enabled => _enabled;
+  /// Status of the tor proxy service
+  TorStatus get status => _status;
+  TorStatus _status = TorStatus.off;
 
   /// Flag to indicate that a Tor circuit is thought to have been established
   /// (true means that Tor has bootstrapped).
-  bool get bootstrapped => _bootstrapped;
-
-  /// Getter for the bootstrapped flag.
   bool _bootstrapped = false;
-
-  /// A stream of Tor events.
-  ///
-  /// This stream broadcast just the port for now (-1 if circuit not established)
-  final StreamController events = StreamController.broadcast();
 
   /// Getter for the proxy port.
   ///
-  /// Returns -1 if Tor is not enabled or if the circuit is not established.
+  /// Throws if Tor is not enabled or if the circuit is not established.
   ///
   /// Returns the proxy port if Tor is enabled and the circuit is established.
   ///
   /// This is the port that should be used for all requests.
   int get port {
-    if (!_enabled) {
-      return -1;
+    if (_proxyPort == null) {
+      throw Exception("");
     }
-    return _proxyPort;
+    return _proxyPort!;
   }
 
   /// The proxy port.
-  int _proxyPort = -1;
-
-  /// Singleton instance of the Tor class.
-  static final Tor _instance = Tor._internal();
-
-  /// Getter for the singleton instance of the Tor class.
-  static Tor get instance => _instance;
-
-  /// Initialize the Tor ffi lib instance if it hasn't already been set. Nothing
-  /// changes if _tor is already been set.
-  ///
-  /// Returns a Future that completes when the Tor service has started.
-  ///
-  /// Throws an exception if the Tor service fails to start.
-  static Future<Tor> init({enabled = true}) async {
-    var singleton = Tor._instance;
-    singleton._enabled = enabled;
-    return singleton;
-  }
-
-  /// Private constructor for the Tor class.
-  Tor._internal() {
-    _lib = load(_libName);
-    print("Instance of Tor created!");
-  }
+  int? _proxyPort;
 
   /// Start the Tor service.
-  Future<void> enable() async {
-    _enabled = true;
-    if (!started) {
-      return await start();
+  ///
+  /// This will start the Tor service and establish a Tor circuit if there
+  /// already hasn't been one established.
+  ///
+  /// Throws an exception if the Tor service fails to start.
+  ///
+  /// Returns a Future that completes when the Tor service has started.
+  Future<void> start({required String torDataDirPath}) async {
+    if (_status != TorStatus.off) {
+      // already starting or running
+      return;
+    }
+
+    try {
+      _status = TorStatus.starting;
+
+      // Set the state and cache directories.
+      final stateDir = await Directory('$torDataDirPath/tor_state').create();
+      final cacheDir = await Directory('$torDataDirPath/tor_cache').create();
+
+      // Generate a random port.
+      final int? newPort = await _getRandomUnusedPort();
+
+      if (newPort == null) {
+        throw Exception("Failed to get random unused port!");
+      }
+
+      // Start the Tor service in an isolate.
+      final int ptr = await Isolate.run(() async {
+        // Load the Tor library.
+        var lib = NativeLibrary(_load(_libName));
+
+        // Start the Tor service.
+        final ptr = lib.tor_start(
+            newPort,
+            stateDir.path.toNativeUtf8() as Pointer<Char>,
+            cacheDir.path.toNativeUtf8() as Pointer<Char>);
+
+        // Throw an exception if the Tor service fails to start.
+        if (ptr == nullptr) {
+          throwRustException(lib);
+        }
+
+        // Return the pointer.
+        return ptr.address;
+      });
+
+      // Set the client pointer and started flag.
+      _clientPtr = Pointer.fromAddress(ptr);
+
+      // Bootstrap the Tor service.
+      _bootstrap();
+
+      // Set the proxy port and change status.
+      _proxyPort = newPort;
+      _status = TorStatus.on;
+    } catch (_) {
+      _status = TorStatus.off;
+      rethrow;
     }
   }
 
-  Future<int> _getRandomUnusedPort({List<int> excluded = const []}) async {
+  /// Bootstrap the Tor service.
+  ///
+  /// This will bootstrap the Tor service and establish a Tor circuit.  This
+  /// function should only be called after the Tor service has been started.
+  ///
+  /// This function will block until the Tor service has bootstrapped.
+  ///
+  /// Throws an exception if the Tor service fails to bootstrap.
+  ///
+  /// Returns void.
+  void _bootstrap() {
+    // Load the Tor library.
+    final lib = NativeLibrary(_lib);
+
+    // Bootstrap the Tor service.
+    _bootstrapped = lib.tor_bootstrap(_clientPtr);
+
+    // Throw an exception if the Tor service fails to bootstrap.
+    if (!_bootstrapped) {
+      throwRustException(lib);
+    }
+  }
+
+  // TODO: this doesn't actually shut tor down
+  // void disable() {
+  //   _status = false;
+  // }
+
+  Pointer<Int> _clientPtr = nullptr;
+
+  Future<int?> _getRandomUnusedPort({List<int> excluded = const []}) async {
     var random = Random.secure();
     int potentialPort = 0;
 
@@ -129,116 +189,16 @@ class Tor {
       }
     }
 
-    return -1;
+    return null;
   }
 
-  /// Start the Tor service.
-  ///
-  /// This will start the Tor service and establish a Tor circuit.
-  ///
-  /// Throws an exception if the Tor service fails to start.
-  ///
-  /// Returns a Future that completes when the Tor service has started.
-  Future<void> start() async {
-    // Send the port to the events stream.
-    events.add(port);
+  // Future<void> restart() async {
+  //   // TODO: arti seems to recover by itself and there is no client restart fn
+  //   // TODO: but follow up with them if restart is truly unnecessary
+  //   // if (enabled && started && circuitEstablished) {}
+  // }
 
-    // Set the state and cache directories.
-    final Directory appSupportDir = await getApplicationSupportDirectory();
-    final stateDir =
-        await Directory('${appSupportDir.path}/tor_state').create();
-    final cacheDir =
-        await Directory('${appSupportDir.path}/tor_cache').create();
-
-    // Generate a random port.
-    int newPort = await _getRandomUnusedPort();
-
-    // Start the Tor service in an isolate.
-    int ptr = await Isolate.run(() async {
-      // Load the Tor library.
-      var lib = NativeLibrary(load(_libName));
-
-      // Start the Tor service.
-      final ptr = lib.tor_start(
-          newPort,
-          stateDir.path.toNativeUtf8() as Pointer<Char>,
-          cacheDir.path.toNativeUtf8() as Pointer<Char>);
-
-      // Throw an exception if the Tor service fails to start.
-      if (ptr == nullptr) {
-        throwRustException(lib);
-      }
-
-      // Return the pointer.
-      return ptr.address;
-    });
-
-    // Set the client pointer and started flag.
-    _clientPtr = Pointer.fromAddress(ptr);
-    _started = true;
-
-    // Bootstrap the Tor service.
-    bootstrap();
-
-    // Set the proxy port and enabled flag.
-    _proxyPort = newPort;
-    _enabled = true;
-  }
-
-  /// Bootstrap the Tor service.
-  ///
-  /// This will bootstrap the Tor service and establish a Tor circuit.  This
-  /// function should only be called after the Tor service has been started.
-  ///
-  /// This function will block until the Tor service has bootstrapped.
-  ///
-  /// Throws an exception if the Tor service fails to bootstrap.
-  ///
-  /// Returns void.
-  void bootstrap() {
-    // Load the Tor library.
-    final lib = NativeLibrary(_lib);
-
-    // Bootstrap the Tor service.
-    _bootstrapped = lib.tor_bootstrap(_clientPtr);
-
-    // Throw an exception if the Tor service fails to bootstrap.
-    if (!bootstrapped) {
-      throwRustException(lib);
-    }
-  }
-
-  // TODO: this doesn't actually shut tor down
-  void disable() {
-    _enabled = false;
-  }
-
-  void restart() {
-    // TODO: arti seems to recover by itself and there is no client restart fn
-    // TODO: but follow up with them if restart is truly unnecessary
-    // if (enabled && started && circuitEstablished) {}
-  }
-
-  Future<void> isReady() async {
-    return await Future.doWhile(
-        () => Future.delayed(const Duration(seconds: 1)).then((_) {
-              // We are waiting and making absolutely no request unless:
-              // Tor is disabled
-              if (!enabled) {
-                return false;
-              }
-
-              // ...or Tor circuit is established
-              if (bootstrapped) {
-                return false;
-              }
-
-              // This way we avoid making clearnet req's while Tor is initialising
-              return true;
-            }));
-  }
-
-  static throwRustException(NativeLibrary lib) {
+  static void throwRustException(NativeLibrary lib) {
     String rustError = lib.tor_last_error_message().cast<Utf8>().toDartString();
 
     throw _getRustException(rustError);
